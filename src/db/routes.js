50624f5320ad955f92b6fe8e4fb323ed4ac4bfdf
Sephora <sephora.panchbaya@epitech.eu>
2022-10-27 13:45:40 +0100

    fix(forms): removing db folder

--- src/db/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {showinsertDemande} from './dbController.js';
-
-const router = express.Router();
-
-router.post('/demande', showinsertDemande);
-
-export default router;
