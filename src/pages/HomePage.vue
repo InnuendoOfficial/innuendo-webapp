0ab44f874acbb9e450226e2caba36f2a9d0a0bb6
Sephora <sephora.panchbaya@epitech.eu>
2024-01-03 12:14:08 +0100

    fixing logos

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -216,7 +216,7 @@
               target="_blank"
               href="https://www.instagram.com/innuendo_official/"
               ><img
-                src="src/assets/insta.png"
+                src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png"
                 width="20"
                 height="20"
                 class="center"
@@ -225,7 +225,7 @@
               target="_blank"
               href="https://www.facebook.com/profile.php?id=100076102473105"
               ><img
-                src="src/assets/fb.png"
+                src="https://www.edigitalagency.com.au/wp-content/uploads/facebook-icon-white-png.png"
                 width="20"
                 height="20"
                 class="center"
@@ -234,7 +234,7 @@
               target="_blank"
               href="https://www.linkedin.com/company/innuendoeip/"
               ><img
-                src="src/assets/lk.png"
+                src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-white-icon.png"
                 width="20"
                 height="20"
                 class="center"
--- src/pages/PatientePage.vue
+++ src/pages/PatientePage.vue
@@ -65,7 +65,7 @@
               target="_blank"
               href="https://www.instagram.com/innuendo_official/"
               ><img
-                src="src/assets/insta.png"
+                src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png"
                 width="20"
                 height="20"
                 class="center"
@@ -74,7 +74,7 @@
               target="_blank"
               href="https://www.facebook.com/profile.php?id=100076102473105"
               ><img
-                src="src/assets/fb.png"
+                src="https://www.edigitalagency.com.au/wp-content/uploads/facebook-icon-white-png.png"
                 width="20"
                 height="20"
                 class="center"
@@ -83,7 +83,7 @@
               target="_blank"
               href="https://www.linkedin.com/company/innuendoeip/"
               ><img
-                src="src/assets/lk.png"
+                src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-white-icon.png"
                 width="20"
                 height="20"
                 class="center"
